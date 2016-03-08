862894fa458386b04456dd6a171db8f7d25ec15b
John Collins <johnlamontcollins@gmail.com>
2016-03-07 19:30:01 -0500

    Revert "client config for fb"
    This reverts commit 66cccf812a673f0e40e874edb834bd9deafb6be5.

--- client/config/client.js
+++ client/config/client.js
@@ -3,5 +3,6 @@ module.exports = {
     map: process.env.MAP_TOKEN || 'mapboxtokenhere'
   },
   FORECAST_API: process.env.FORECAST_API || 'http://localhost:8081',
-  wunderground: process.env.WUNDERGROUND_KEY || 'wundergroundapikeyhere'
+  wunderground: process.env.WUNDERGROUND_KEY || 'wundergroundapikeyhere',
+  FIREBASE_URL: process.env.FIREBASE_URL || 'firebaseendpointhere'
 };
