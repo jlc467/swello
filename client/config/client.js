66cccf812a673f0e40e874edb834bd9deafb6be5
John Collins <johnlamontcollins@gmail.com>
2016-03-07 19:27:38 -0500

    client config for fb

--- client/config/client.js
+++ client/config/client.js
@@ -3,6 +3,5 @@ module.exports = {
     map: process.env.MAP_TOKEN || 'mapboxtokenhere'
   },
   FORECAST_API: process.env.FORECAST_API || 'http://localhost:8081',
-  wunderground: process.env.WUNDERGROUND_KEY || 'wundergroundapikeyhere',
-  FIREBASE_URL: process.env.FIREBASE_URL || 'firebaseendpointhere'
+  wunderground: process.env.WUNDERGROUND_KEY || 'wundergroundapikeyhere'
 };
